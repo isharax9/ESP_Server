b59ac7ed1e0a896f6ec259dab89d2c88dc63331f
mac_knight141 <123813453+isharax9@users.noreply.github.com>
2024-11-01 16:19:56 +0530

    ON and Off buttons are now working with LED✅

--- Arduino/sketch_nov1a/sketch_nov1a.ino
+++ Arduino/sketch_nov1a/sketch_nov1a.ino
@@ -6,9 +6,10 @@ const char *ssid = "macNaWifi-2.4G";
 const char *password = "IshLak@5377.";
 
 
-
 void setup() {
   Serial.begin(115200);
+
+  pinMode(13, OUTPUT);
 
   WiFi.begin(ssid, password);
   Serial.print("Connecting to WiFi");
@@ -30,10 +31,24 @@ void loop() {
   NetworkClient client = server.accept();  // listen for incoming clients
 
   if (client) {
+
+    String requestText = "";
+
     while (client.connected()) {
       if (client.available()) {
         char c = client.read();
-        Serial.write(c);
+        requestText += c;
+
+        if (c == '\n') {
+          Serial.println(requestText);
+
+          if (requestText.startsWith("GET /?status=1")) {
+            digitalWrite(13, HIGH);
+          } else {
+            digitalWrite(13, LOW);
+          }
+          break;
+        }
       }
     }
     client.stop();
